1694310e8c2cd81573189bec41763bc25f7d7ba0
lynnagidza <lynnagidza@gmail.com>
2023-08-04 08:54:46 +0300

    fixed documentation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -31,10 +31,12 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """initializes the class"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """filters values in incoming log records"""
         msg = super().format(record)
         return filter_datum(self.fields, self.REDACTION,
                             msg, self.SEPARATOR)
