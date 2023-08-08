f3bdda3d7059689ef659f3cde1b4945190186dcd
lynnagidza <lynnagidza@gmail.com>
2023-08-08 12:26:44 +0300

    mandatory tasks

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -3,6 +3,7 @@
 """
 from flask import jsonify, abort
 from api.v1.views import app_views
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -20,7 +21,24 @@ def stats() -> str:
     Return:
       - the number of each objects
     """
-    from models.user import User
-    stats = {}
-    stats['users'] = User.count()
-    return jsonify(stats)
+    stats2 = {}
+    stats2['users'] = User.count()
+    return jsonify(stats2)
+
+
+@app_views.route('/unauthorized/', strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - abort(401)
+    """
+    abort(401)
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - abort(403)
+    """
+    abort(403)
--- README.md
+++ README.md
@@ -8,6 +8,14 @@ This project is about:
 - How to encrypt a password and check the validity of an input password
 - How to authenticate to a database using environment variables
 
+### 0x01-Basic_authentication
+This project is about:
+- What authentication means
+- What Base64 is
+- How to encode a string in Base64
+- What Basic authentication means
+- How to send the Authorization header
+
 ## Usage
 To use the projects in this repository, follow the instructions provided in each respective directory.
 
