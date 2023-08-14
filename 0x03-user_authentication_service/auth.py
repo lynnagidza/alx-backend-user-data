60662a5ee7db07b12a41ba0da5e5fa4ec24303c1
lynnagidza <lynnagidza@gmail.com>
2023-08-14 18:06:24 +0300

    tasks 11 - 19

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """ Auth module """
+from uuid import uuid4
 import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
-from uuid import uuid4
 
 
 class Auth:
@@ -45,6 +45,48 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> str:
+        """ Takes mandatory session_id string argument and returns the User
+        associated with the session ID.
+        """
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """ Takes mandatory user_id integer argument and returns None.
+        """
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except NoResultFound:
+            return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """ Takes mandatory email string argument and returns
+        the user reset token as a string.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except NoResultFound:
+            raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """ Takes mandatory reset_token string argument and a password string
+        argument and returns None.
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_password = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=hashed_password,
+                                 reset_token=None)
+        except NoResultFound:
+            raise ValueError
 
 
 def _hash_password(password: str) -> str:
